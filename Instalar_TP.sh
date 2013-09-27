7c26275e878b3c9228e0b3b4d29593197c6899ba
pablo.gabriel.federico <pablo.gabriel.federico@gmail.com>
2013-09-27 15:37:27 +0000

    cambiando un par de cosas

--- Instalar_TP.sh
+++ Instalar_TP.sh
@@ -7,17 +7,21 @@
 #Variables de mi script
 usuario='whoami'
 ruta_usuario="/home/$usuario"
-grupo="./grupo8" #deberia ser el directorio creado para el grupo8 a manopla
-CONFDIR="$grupo/conf" #seteo la variable confdir. NO CAMBIA
+grupo="./grupo8" #deberia ser el directorio creado para el grupo8 a mano
+CONFDIR="$grupo/conf" 
 Instlog="$CONFDIR/Instalar_TP$LOGEXT" #ruta a el log del script
 estado="COMPLETO" #variable para controlar el estado de la instalacion
-DATASIZE=100 #variable para el tamaño en MB de el directorio ARRIDIR
+DATASIZE=100 #variable para el tamaño en MB de los arribos
 LOGEXT=".log" #extension por defecto de los archivos de log
 COMANDO="Instalar_Tp" #comando de mi script
 
 
+mkdir -p "$CONFDIR" #creo el directorio ./grupo8/conf 
+
 
 #Por defecto asumo estos directorios 
+function DirectoriosDefault(){
+
 BINDIR="$grupo/bin"
 MAEDIR="$grupo/mae"
 ARRIDIR="$grupo/arribos"
@@ -27,12 +31,11 @@ REPODIR="$grupo/listados"
 PROCDIR="$grupo/procesados"
 LOGDIR="$grupo/log"
 
-
+}
 
 
 ## Funcion que verifica si existe la carpeta grupo8/conf en el sistema
 function estaConf(){
-
 if [ -d $CONFDIR ]
 then
 return 1
@@ -40,23 +43,23 @@ else return 0
 fi
 }
 
-
-# Funcion que crear CONFDIR si no esta creada en el sistema
-function crearDir{ 
-
-if [estaConf == "0"]
-then
-	mkdir "$CONFDIR"
-fi
+#Para finalizar mi script
+function FIN(){
+exit
 }
 
 
-# Paso 1, 2 y 3
-# Funcion que inicia el log (debe llamarse si
-# no fue creada la isntalacion de ante mano ( si no existe la carpeta conf)
-function InicioLogInstalacion(){
 
-touch $Instlog
+# Paso 1, 2 y 3
+# Funcion que inicia el log. Lo crea si no estaba.
+
+function InicioLogInstalacion(){
+if [-f "$Instlog"];
+then
+echo "Existe "$Instlog""
+else
+touch "$Instlog" #aca hay que darle permisos de escritura??VER
+fi
 
 echo "Inicio de Ejecución" 
 echo "Log del comando Instalar_TP:$Instlog"
@@ -66,11 +69,16 @@ echo "DIrectorio de configuracion:$CONFDIR"
 
 
 
+#Paso 4. FALTA (comprobacion de el paquete este todo instalado)
+
+
+
 # Paso 4.1
-# Muestra el estado del sistema por pantalla y en el log
+# Muestra el estado del sistema. Llamar si esta completo el paquete.
+
 function MostrarMensajeInicial(){
 echo "TP SO7508 Segundo Cuatrimestre 2013. Tema B Copyright Grupo 08"
-echo "Libreria del Sistema: $CONFDIR /n"
+echo "Libreria del Sistema: $CONFDIR"
 ls "$CONFDIR"
 echo "Ejecutables:$BINDIR"
 ls "$BINDIR"
