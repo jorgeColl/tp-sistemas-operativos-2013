d32ac660af80cc966b0f49649dd0323e223c43ff
juandausa <juandausa@gmail.com>
2013-10-01 03:16:39 +0000

    Se corrigio el error que no permitia matar procesos con más de una instancia.

--- Matar_D.sh
+++ Matar_D.sh
@@ -2,7 +2,8 @@
 #Procedimiento en bash para la finalizacion brusa de un proceso
 #Parametros de ejecucion: ./Matar_B nombreProceso o ./Matar_B idProceso
 
-#Bugs conocidos:
+#Bugs conocidos: 
+#Porque tantos sed sed '/^$/d'? Porque cada vez que paso a una variable se me mueren los fines de linea y tengo que arrancar de nuevo.
 
 # ----------------------------------
 #!/bin/bash
@@ -13,22 +14,27 @@
 		exit -1
 	fi
 	cantidad=`ps | grep -E "$1"`
-	#echo ${cantidad:-0}
-	cantidadEncontrada=`echo $cantidad | sed '/./!d' | wc -l`
+	#echo ${cantidad}
+	cantidadEncontrada=`echo $cantidad | sed '/^$/d' | wc -l`
 	#echo ${cantidadEncontrada:-0}
 	if [ ${cantidadEncontrada:-0} -eq 0 ]; then
 		#No se esta ejecutando ese proceso
 		echo "El proceso: $1 no se esta ejecutando" #Escribir en log
 	else 
 		#Saco el resto de la linea
-		cantidad=`echo ${cantidad% *}`
-		cantidad=`echo ${cantidad% *}`
-		cantidad=`echo ${cantidad% *}`
-		#Se necesitan los 3, NO es un error.
-		
-		#echo $cantidad
-		kill ${cantidad:-0}
-		#Escribir en log
-		echo "El proceso: $1 ha finalizado" #Escribir en log
+		ps | grep -E "$1" | sed '/^$/d' > archivoAuxiliarMatar_D.tmp
+		while read linea
+		do
+			p_id=`echo ${linea% *}`
+			p_id=`echo ${p_id% *}`
+			p_id=`echo ${p_id% *}`	
+			#Se necesitan los 3, NO es un error.
+
+			#echo $cantidad
+			kill ${p_id:-0}
+			#Escribir en log
+			echo "El proceso: $1 ha finalizado" #Escribir en log
+		done < archivoAuxiliarMatar_D.tmp
+		rm archivoAuxiliarMatar_D.tmp
 	fi
 	exit 0
