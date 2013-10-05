8aeea91a422e33b92ad089ec7a2ec83c1c775315
collinetjorge <collinetjorge@gmail.com>
2013-10-05 22:32:31 +0000

    permite verificar si el entorno fue inicializado

---
+++ EstaInicializado.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Funcion que se encarga de verificar si Iniciar.sh fue ejecutado con anterioridad, devuelve 1 si NO esta inicializado y 0 si SI lo esta
+
+#Esto viene joya para cuando una persona quiere hacer:
+#if ./EstaInicializad.sh
+#then echo "wiiii"
+#else echo "buuuu"
+#fi
+
+function EstaInicializado {
+if [ "$BINDIR" = "" ]||[ "$MAEDIR" = "" ] || [ "$ARRIDIR" = "" ] || [ "$ACEPDIR" = "" ] || [ "$RECHDIR" = "" ] || [ "$REPODIR" = "" ] || [ "$PROCDIR" = "" ] || [ "$LOGDIR" = "" ] || [ "$DATASIZE" = "" ] || [ "$LOGSIZE" = "" ];
+then 
+	echo "no esta inicializado"
+	return "1"
+else 
+	echo "esta inicializado"
+	return "0"
+fi
+}
+EstaInicializado
