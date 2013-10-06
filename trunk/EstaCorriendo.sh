2699078d5b4911fb9493c6f338d9331f4f971692
collinetjorge@gmail.com <collinetjorge@gmail.com@fd57c70f-2bc0-413e-ca4d-84ebd442f837>
2013-10-06 21:20:19 +0000

    version 2 de EstaCorriendo

--- trunk/EstaCorriendo.sh
+++ trunk/EstaCorriendo.sh
@@ -1,16 +1,19 @@
 # Proceso en bas que devuelve 0 si el proceso solicitado esta corrirendo 1 o 0 veces. 
 # Retorna 1 si el proceso se esta ejcutando mas de una vez
-# Invocacion: ./EstaCorriendo.sh procesoAConsultar
+# Invocacion: ./EstaCorriendo.sh procesoAConsultar cantidadDeVeces
 
 # --------------------------------------- 
 #!/bin/bash
+function EstaCorriendo {
 	comandoSolicitante=`echo ${1##*/}`
 	cantidadEjecuciones=`ps -e | grep "$comandoSolicitante$" | wc -l`
-	if [ $cantidadEjecuciones -gt 1 ]; then
-		echo "Esta corriendo MAS de una vez"
+	if [ $cantidadEjecuciones -gt $2 ]; then
+		echo "Esta corriendo MAS de $2 vez/veces"
 		return 1
 	else
-		echo "Esta corriendo una o MENOS veces"
+		echo "Esta corriendo $2 o MENOS de $2 veces"
 		return 0
 	fi
 	return 0
+}
+EstaCorriendo $1 $2
