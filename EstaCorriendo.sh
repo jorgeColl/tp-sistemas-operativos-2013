a8dce4fe19abe467bce5aec00cb08f6a3202b10c
juandausa <juandausa@gmail.com>
2013-10-06 21:48:47 +0000

    Se utiliza una cantidad de veces ingresada como parametro, de lo contrario se interpreta que solo debe verificar si el programa se esta ejecutando.

--- EstaCorriendo.sh
+++ EstaCorriendo.sh
@@ -1,19 +1,27 @@
-# Proceso en bas que devuelve 0 si el proceso solicitado esta corrirendo 1 o 0 veces. 
-# Retorna 1 si el proceso se esta ejcutando mas de una vez
+# Proceso en bash que devuelve 1 si el proceso solicitado esta corrirendo n veces o mas. 
+# Retorna 0 de lo contrario.
 # Invocacion: ./EstaCorriendo.sh procesoAConsultar cantidadDeVeces
 
 # --------------------------------------- 
 #!/bin/bash
+
 function EstaCorriendo {
-	comandoSolicitante=`echo ${1##*/}`
+	comandoSolicitante=`echo "${1##*/}"`
 	cantidadEjecuciones=`ps -e | grep "$comandoSolicitante$" | wc -l`
-	if [ $cantidadEjecuciones -gt $2 ]; then
-		echo "Esta corriendo MAS de $2 vez/veces"
+	if [ $cantidadEjecuciones -ge $2 ]; then
+		#echo "Esta corriendo $2 o MAS vez/veces"
 		return 1
 	else
-		echo "Esta corriendo $2 o MENOS de $2 veces"
+		#echo "Esta corriendo MENOS de $2 veces"
 		return 0
 	fi
 	return 0
 }
-EstaCorriendo $1 $2
+if [ $# -eq 2 ]; then
+	veces=$2
+else
+	veces=0
+fi
+if [ $# -ge 1 ]; then
+	EstaCorriendo $1 $veces
+fi
